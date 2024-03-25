1e6551834a917b4238b7d01a3a4e96e62a1d48ee
kushu001 <yj_tao123@sina.com>
2024-03-25 16:46:31 +0800

    initial project preview edition

---
+++ src/App.vue
@@ -0,0 +1,175 @@
+<template>
+  <el-container class="layout-container-demo" :style="`height: ${windowHeight}px`">
+    <el-aside width="{isCollapse ? '65px' : '200px'}">
+        <div style="text-align: center;">
+          <div v-if="!isCollapse" style="display: flex;flex-direction: column; justify-content: center;height: 60px;">
+            <div>Nebula</div>
+            <div style="font-size: 10px; color:blueviolet">Preview Edition</div>
+          </div>
+          <div v-if="isCollapse" style="display: flex;flex-direction: column; justify-content: center;height: 60px;">
+            <div>NE</div>
+          </div>
+          
+        </div>
+        <el-scrollbar :style="`height: ${windowHeight - 60}px`">
+          <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            :collapse="isCollapse"
+            @open="handleOpen"
+            @close="handleClose"
+            :style="`min-height: ${windowHeight - 60}px`"
+          >
+            <el-sub-menu index="1">
+              <template #title>
+                <el-icon><location /></el-icon>
+                <span>Navigator One</span>
+              </template>
+              <el-menu-item-group>
+                <template #title><span>Group One</span></template>
+                <el-menu-item index="1-1">item one</el-menu-item>
+                <el-menu-item index="1-2">item two</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="Group Two">
+                <el-menu-item index="1-3">item three</el-menu-item>
+              </el-menu-item-group>
+              <el-sub-menu index="1-4">
+                <template #title><span>item four</span></template>
+                <el-menu-item index="1-4-1">item one</el-menu-item>
+              </el-sub-menu>
+            </el-sub-menu>
+            <el-menu-item index="2">
+              <el-icon><icon-menu /></el-icon>
+              <template #title>Navigator Two</template>
+            </el-menu-item>
+            <el-menu-item index="3" disabled>
+              <el-icon><document /></el-icon>
+              <template #title>Navigator Three</template>
+            </el-menu-item>
+            <el-menu-item index="4">
+              <el-icon><setting /></el-icon>
+              <template #title>Navigator Four</template>
+            </el-menu-item>
+          </el-menu>
+        </el-scrollbar>
+    </el-aside>
+
+    <el-container>
+      <el-header style="font-size: 12px;height: 90px;">
+        <div style="display: flex;flex-direction: row; justify-content: space-between">
+          <div style="display: flex;flex-direction: row; justify-content: space-between">
+            <div style="display: flex;flex-direction: column; justify-content: flex-end;height: 60px;">
+              <el-icon :size="25" @click="isCollapse= !isCollapse" style="cursor: pointer;" color="#000">
+                <component :is="!isCollapse ? Fold : Expand" />
+              </el-icon>
+            </div>
+            <el-menu
+              :default-active="activeIndex"
+              mode="horizontal"
+              @select="handleSelect"
+            >
+              <el-menu-item index="1">Processing Center</el-menu-item>
+              <el-sub-menu index="2">
+                <template #title>Workspace</template>
+                <el-menu-item index="2-1">item one</el-menu-item>
+                <el-menu-item index="2-2">item two</el-menu-item>
+                <el-menu-item index="2-3">item three</el-menu-item>
+                <el-sub-menu index="2-4">
+                  <template #title>item four</template>
+                  <el-menu-item index="2-4-1">item one</el-menu-item>
+                  <el-menu-item index="2-4-2">item two</el-menu-item>
+                  <el-menu-item index="2-4-3">item three</el-menu-item>
+                </el-sub-menu>
+              </el-sub-menu>
+              <el-menu-item index="3" disabled>Info</el-menu-item>
+              <el-menu-item index="4">Orders</el-menu-item>
+            </el-menu>
+          </div>
+          <div style="display: flex;flex-direction: column; justify-content: center;height: 60px;">
+            <div>
+              <el-dropdown>
+                <el-icon style="margin-right: 8px; margin-top: 1px"
+                  ><setting
+                /></el-icon>
+                <template #dropdown>
+                  <el-dropdown-menu>
+                    <el-dropdown-item>View</el-dropdown-item>
+                    <el-dropdown-item>Add</el-dropdown-item>
+                    <el-dropdown-item>Delete</el-dropdown-item>
+                  </el-dropdown-menu>
+                </template>
+              </el-dropdown>
+              <span>Tom</span>
+            </div>
+          </div>
+        </div>
+        <div style=" height: 30px;display: flex;flex-direction: column; justify-content: center;">
+          <el-breadcrumb separator="/">
+            <el-breadcrumb-item :to="{ path: '/' }">homepage</el-breadcrumb-item>
+            <el-breadcrumb-item
+              ><a href="/">promotion management</a></el-breadcrumb-item
+            >
+            <el-breadcrumb-item>promotion list</el-breadcrumb-item>
+            <el-breadcrumb-item>promotion detail</el-breadcrumb-item>
+          </el-breadcrumb>
+        </div>
+      </el-header>
+      <el-main>
+        <el-scrollbar>
+          <el-table :data="tableData">
+            <el-table-column prop="date" label="Date" width="140" />
+            <el-table-column prop="name" label="Name" width="120" />
+            <el-table-column prop="address" label="Address" />
+          </el-table>
+        </el-scrollbar>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted, onUnmounted } from 'vue'
+import { Menu as IconMenu, Setting } from '@element-plus/icons-vue'
+import {
+  Document,
+  Location,
+  Expand, 
+  Fold
+} from '@element-plus/icons-vue'
+const windowHeight = ref(window.innerHeight)
+const isCollapse = ref(false)
+
+const handleOpen = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+const handleClose = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+
+function handleResize() {
+  windowHeight.value = window.innerHeight;
+}
+const activeIndex = ref('1')
+
+const handleSelect = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+onMounted(() => {
+  window.addEventListener('resize', handleResize);
+  handleResize(); // 初始化获取高度
+});
+ 
+onUnmounted(() => {
+  window.removeEventListener('resize', handleResize);
+});
+
+const item = {
+  date: '2016-05-02',
+  name: 'Tom',
+  address: 'No. 189, Grove St, Los Angeles',
+}
+const tableData = ref(Array.from({ length: 20 }).fill(item))
+</script>
+
+<style scoped>
+</style>
