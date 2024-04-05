7c35c09e040fd718ed636a41108afef4d7ab41c9
kushu001 <yj_tao123@sina.com>
2024-04-05 17:28:26 +0800

    [feat] 添加路由导航守卫代码，方便控制什么情况下可以访问对应的路由

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
 import './style.css'
 import App from './App.vue'
-import router from './router'
+import router from './permission'
 
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
