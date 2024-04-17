02f08908fc3813384efb1933defed603fe3b27ad
kushu001 <yj_tao123@sina.com>
2024-04-17 16:44:07 +0800

    [feat] 左侧菜单分离，抽取成组件

---
+++ src/layout/components/Asider/index.vue
@@ -0,0 +1,85 @@
+<template>
+      <el-aside width="auto" style="border-right: 1px solid #dcdfe6;height: 100%;display: flex;flex-direction: column;">
+      <div style="text-align: center;height: 60px;">
+        <div v-if="!isCollapse" style="display: flex;flex-direction: column; justify-content: center;height: 60px;">
+          <div>Nebula</div>
+          <div style="font-size: 10px; color:blueviolet">Preview Edition</div>
+        </div>
+        <div v-if="isCollapse" style="display: flex;flex-direction: column; justify-content: center;height: 60px;">
+          <div>NE</div>
+        </div>
+      </div>
+      <el-scrollbar style="flex: 1;">
+        <el-menu
+          ref="sideMenu"
+          default-active="1"
+          :collapse="isCollapse"
+          @open="handleOpen"
+          @close="handleClose"
+          class="el-menu-vertical-demo"
+          :style="`border-right:0px;`"
+        >
+          <el-sub-menu index="1">
+            <template #title>
+              <el-icon><location /></el-icon>
+              <span>日志管理</span>
+            </template>
+            <el-menu-item-group>
+              <template #title><span>Group One</span></template>
+              <router-link to="/"><el-menu-item index="1-1">item one</el-menu-item></router-link>
+              <router-link to="/item21"><el-menu-item index="1-2">item two</el-menu-item></router-link>
+            </el-menu-item-group>
+            <el-menu-item-group title="Group Two">
+              <el-menu-item index="1-3">item three</el-menu-item>
+            </el-menu-item-group>
+            <el-sub-menu index="1-4">
+              <template #title><span>item four</span></template>
+              <el-menu-item index="1-4-1">item one</el-menu-item>
+            </el-sub-menu>
+          </el-sub-menu>
+          <el-menu-item index="2">
+            <el-icon><icon-menu /></el-icon>
+            <template #title>系统管理</template>
+          </el-menu-item>
+          <el-menu-item index="3" disabled>
+            <el-icon><document /></el-icon>
+            <template #title>开发管理</template>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <el-icon><setting /></el-icon>
+            <template #title>基础管理</template>
+          </el-menu-item>
+        </el-menu>
+      </el-scrollbar>
+      <div :style="`display:flex; display: -webkit-flex;-webkit-justify-content: center;box-sizing: border-box;justify-content: center;height: ${!isCollapse ? '30' : '45'}px;position: relative;`">
+        <span :style="`font-size: 10px;color: #909399;height: ${!isCollapse ? '30' : '45'}px; vertical-align: middle;position: absolute; bottom: 0px; text-align: center;`">
+            Copyright © {{ new Date().getFullYear() }} By NOSG
+        </span>
+      </div>
+    </el-aside>
+</template>
+<script lang="ts" setup>
+import {
+  Location,
+  Menu as IconMenu
+} from '@element-plus/icons-vue'
+defineProps({
+  isCollapse: {
+    type: Boolean,
+    default: false
+  },
+})
+
+const handleOpen = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+const handleClose = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+</script>
+<style>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 250px;
+    min-height: 400px;
+  }
+</style>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,63 +1,6 @@
 <template>
   <el-container style="height: 100%;">
-    <el-aside width="auto" style="border-right: 1px solid #dcdfe6;height: 100%;display: flex;flex-direction: column;">
-      <div style="text-align: center;height: 60px;">
-        <div v-if="!isCollapse" style="display: flex;flex-direction: column; justify-content: center;height: 60px;">
-          <div>Nebula</div>
-          <div style="font-size: 10px; color:blueviolet">Preview Edition</div>
-        </div>
-        <div v-if="isCollapse" style="display: flex;flex-direction: column; justify-content: center;height: 60px;">
-          <div>NE</div>
-        </div>
-      </div>
-      <el-scrollbar style="flex: 1;">
-        <el-menu
-          ref="sideMenu"
-          default-active="1"
-          :collapse="isCollapse"
-          @open="handleOpen"
-          @close="handleClose"
-          class="el-menu-vertical-demo"
-          :style="`border-right:0px;`"
-        >
-          <el-sub-menu index="1">
-            <template #title>
-              <el-icon><location /></el-icon>
-              <span>日志管理</span>
-            </template>
-            <el-menu-item-group>
-              <template #title><span>Group One</span></template>
-              <router-link to="/"><el-menu-item index="1-1">item one</el-menu-item></router-link>
-              <router-link to="/item21"><el-menu-item index="1-2">item two</el-menu-item></router-link>
-            </el-menu-item-group>
-            <el-menu-item-group title="Group Two">
-              <el-menu-item index="1-3">item three</el-menu-item>
-            </el-menu-item-group>
-            <el-sub-menu index="1-4">
-              <template #title><span>item four</span></template>
-              <el-menu-item index="1-4-1">item one</el-menu-item>
-            </el-sub-menu>
-          </el-sub-menu>
-          <el-menu-item index="2">
-            <el-icon><icon-menu /></el-icon>
-            <template #title>系统管理</template>
-          </el-menu-item>
-          <el-menu-item index="3" disabled>
-            <el-icon><document /></el-icon>
-            <template #title>开发管理</template>
-          </el-menu-item>
-          <el-menu-item index="4">
-            <el-icon><setting /></el-icon>
-            <template #title>基础管理</template>
-          </el-menu-item>
-        </el-menu>
-      </el-scrollbar>
-      <div :style="`display:flex; display: -webkit-flex;-webkit-justify-content: center;box-sizing: border-box;justify-content: center;height: ${!isCollapse ? '30' : '45'}px;position: relative;`">
-        <span :style="`font-size: 10px;color: #909399;height: ${!isCollapse ? '30' : '45'}px; vertical-align: middle;position: absolute; bottom: 0px; text-align: center;`">
-            Copyright © {{ new Date().getFullYear() }} By NOSG
-        </span>
-      </div>
-    </el-aside>
+    <asider :isCollapse="isCollapse" />
     <el-container>
       <el-header style="font-size: 12px;height: 90px;">
         <div class="container">
@@ -71,24 +14,11 @@
           <div class="middle">
             <el-menu
               :default-active="activeIndex"
-              class="el-menu-demo"
               mode="horizontal"
               @select="handleSelect"
             >
               <el-menu-item index="1"><el-icon><location /></el-icon>系统管理</el-menu-item>
               <el-menu-item index="2">开发管理</el-menu-item>
-              <!-- <el-sub-menu index="2">
-                <template #title>Workspace</template>
-                <el-menu-item index="2-1">item one</el-menu-item>
-                <el-menu-item index="2-2">item two</el-menu-item>
-                <el-menu-item index="2-3">item three</el-menu-item>
-                <el-sub-menu index="2-4">
-                  <template #title>item four</template>
-                  <el-menu-item index="2-4-1">item one</el-menu-item>
-                  <el-menu-item index="2-4-2">item two</el-menu-item>
-                  <el-menu-item index="2-4-3">item three</el-menu-item>
-                </el-sub-menu>
-              </el-sub-menu> -->
               <el-menu-item index="3">日志管理</el-menu-item>
             </el-menu>
           </div>
@@ -122,23 +52,17 @@
 
 <script lang="ts" setup>
 import { ref, onMounted, onUnmounted } from 'vue'
-import { Menu as IconMenu, Setting } from '@element-plus/icons-vue'
 import {
-  Document,
+  Setting,
   Location,
   Expand, 
   Fold
 } from '@element-plus/icons-vue'
-import TabsNav from './components/TabsNav'
+import TabsNav from './components/TabsNav/index.vue'
+import Asider from './components/Asider/index.vue'
 const windowHeight = ref(window.innerHeight)
 const windowWidth = ref(window.innerWidth)
 const isCollapse = ref(false)
-const handleOpen = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath)
-}
-const handleClose = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath)
-}
 
 function handleResize() {
   windowHeight.value = window.innerHeight;
@@ -154,7 +78,7 @@ const activeIndex = ref('1')
 const handleSelect = (key: string, keyPath: string[]) => {
   console.log(key, keyPath)
 }
-const sideMenuRef = ref(null);
+
 onMounted(() => {
   window.addEventListener('resize', handleResize);
   handleResize(); // 初始化获取高度
@@ -190,8 +114,5 @@ onUnmounted(() => {
     min-width: 0; /* 避免某些情况下内容过少时，元素不收缩的问题 */
 }
 
-.el-menu-vertical-demo:not(.el-menu--collapse) {
-    width: 250px;
-    min-height: 400px;
-  }
+
 </style>
